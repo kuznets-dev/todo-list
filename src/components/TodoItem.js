b57a28f112b23d251d75ca0ded5f69470fba389d
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-05-27 13:15:38 +0300

    Add function renaming todo

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,31 @@
+import React, {useState} from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
-import { Checkbox, Grid, ListItemText } from '@material-ui/core';
+import { Checkbox, Grid, ListItemText, TextField } from '@material-ui/core';
 import DoneIcon from '@material-ui/icons/Done';
 import DoneAllIcon from '@material-ui/icons/DoneAll';
 
-function TodoItem({ todo, removeTodo, toggleTodo }) {
+function TodoItem({ todo, removeTodo, toggleTodo, changeNameTodo }) {
+
+    const [toggleNameTodo, setToggleNameTodo] = useState(false);
+    const [todoName, setTodoName] = useState(todo.task);
+
+    const handleKeyDown = (id, e) => {
+        if (e.keyCode === 13) {
+            e.preventDefault();
+
+            if (todoName !== '') {
+                setToggleNameTodo(false);
+                changeNameTodo(id, todoName);
+            }
+        }
+        if (e.keyCode === 27) {
+            setToggleNameTodo(false);
+            setTodoName(todo.task);
+        }
+    }
+
     return (
         <ListItem>
             <Grid
@@ -21,9 +41,20 @@ function TodoItem({ todo, removeTodo, toggleTodo }) {
                     checked={todo.status} />
                 </Grid>
                 <Grid item xs={8}>
-                    <ListItemText>
-                        {todo.task}
-                    </ListItemText>
+                    {toggleNameTodo 
+                        ? <TextField 
+                            multiline={true}
+                            value={todoName}
+                            autoFocus={true}
+                            variant='outlined'
+                            onChange={(e) => setTodoName(e.target.value)}
+                            onKeyDown={(e) => handleKeyDown(todo.id, e)} />
+                        :   <ListItemText 
+                                primary={todoName}
+                                style={{overflowWrap: 'break-word'}}
+                                multiline='true'
+                                onClick={() => setToggleNameTodo(true)} />
+                    }
                 </Grid>
                 <Grid item xs={2}>
                     <ListItemText>
