87fda38d6f634e469d4ff7c169a1aece32a3ce16
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-05-27 18:42:51 +0300

    Add Fetch API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
@@ -6,41 +6,58 @@ import TodoFilter from './components/TodoFilter';
 import TodoSort from './components/TodoSort';
 import { Grid } from '@material-ui/core';
 import Pagination from './components/Pagination';
+import axios from './axiosConfig';
 
 function App() {
 
     // State
+    const GETurl = '/v1/tasks/1';
+    const POSTurl = '/v1/task/1';
     const [todos, setTodos] = useState([]);
     const [todoStatus, setTodoStatus] = useState('all');
     const [todoSort, setTodoSort] = useState(true);
     const [currentPage, setCurrentpage] = useState(1);
-    const [perPage] = useState(5);  
+    const [perPage] = useState(5);
+    
+    // Fetch API
+    // GET
+    const fetchTodos = useCallback(async () => {
+        const response = await axios.get(GETurl);
+        console.log(response.data);
+        setTodos(response.data)
+    }, [setTodos]);
 
+    useEffect(() => {
+        fetchTodos()
+    }, [fetchTodos])
+
+    // POST
     // Add new todo
-    const addTodo = (todoName) => {
-        if (todoName) {
-            const newItem = {
-                id: Math.floor(Math.random() * (1000 - 1 + 1)) +1,
-                task: todoName,
-                date: new Date().toLocaleDateString(),
-                time: new Date().getTime(),
-                status: false
-            }
-            setTodos([...todos, newItem])
-        }
+    const addTodo = async (todoName) => {
+        await axios.post(POSTurl, 
+        {
+            name: todoName,
+            done: false,
+        });
+        await fetchTodos();
     }
 
+    // DELETE
     // Remove todo
-    const removeTodo = (id) => {
-        setTodos(todos.filter(todo => todo.id !== id))
+    const removeTodo = async (id) => {
+        await axios.delete(`${POSTurl}/${id}`);
+        await fetchTodos();
     }
 
+    // PATCH
     // Change status todo
-    const toggleTodo = (id) => {        
-        const newTodos = [...todos];        
-        const todoId = newTodos.findIndex(todo => todo.id === id);        
-        newTodos[todoId].status = !newTodos[todoId].status;
-        setTodos(newTodos);
+    const toggleTodo = async (todo) => {
+        await axios.patch(`${POSTurl}/${todo.uuid}`,
+        {
+            name: todo.name,
+            done: !todo.done
+        });
+        await fetchTodos();
     }
 
     // Rename todo
@@ -51,38 +68,39 @@ function App() {
     }
 
     // Filtering and sorting
-    const filterTodos = useMemo(() => {
-        const newTodos = [...todos];
+    // const filterTodos = useMemo(() => {
+    //     const newTodos = [...todos];
 
-        const filteredTodos = newTodos.filter(todo => {
-            switch (todoStatus) {
-                case 'all':
-                    return todo;
-                case 'done':
-                    return todo.status === true;
-                default:
-                    return todo.status === false;
-            }
-        })
+    //     const filteredTodos = newTodos.filter(todo => {
+    //         switch (todoStatus) {
+    //             case 'all':
+    //                 return todo;
+    //             case 'done':
+    //                 return todo.status === true;
+    //             default:
+    //                 return todo.status === false;
+    //         }
+    //     })
 
-        const sortBy = filteredTodos.sort((a, b) => {
-            if (!todoSort) {
-                return b.time - a.time;
-            }
-            return a.time - b.time;
-        })
+    //     const sortBy = filteredTodos.sort((a, b) => {
+    //         if (!todoSort) {
+    //             return b.time - a.time;
+    //         }
+    //         return a.time - b.time;
+    //     })
 
-        return sortBy;
+    //     return sortBy;
 
-    }, [todos, todoStatus, todoSort]);
+    // }, [todos, todoStatus, todoSort]);
 
     // Pagination
     const paginationTodo = useMemo(() => {
+        const filterTodos = [...todos];
         const indexOfLastPost = currentPage * perPage;
         const indexOfFirstPost = indexOfLastPost - perPage;
         const currentTodos = filterTodos.slice(indexOfFirstPost, indexOfLastPost);
         return currentTodos;
-    }, [currentPage, perPage, filterTodos]);
+    }, [currentPage, perPage, todos]);
 
     return (
         <div className="wrapper">
@@ -100,7 +118,7 @@ function App() {
                 alignItems="center">
                 <Grid>
                     <TodoFilter
-                        filterTodos={filterTodos}
+                        filterTodos={paginationTodo}
                         todoStatus={todoStatus}
                         setTodoStatus={setTodoStatus}
                     />
@@ -120,7 +138,7 @@ function App() {
             />
             {(paginationTodo.length > 0) && 
                 <Pagination
-                    totalTodos={filterTodos.length}
+                    totalTodos={paginationTodo.length}
                     perPage={perPage}
                     currentPage={currentPage}
                     setCurrentpage={setCurrentpage}>
