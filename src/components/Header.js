09791e7b8f49c4ed38124a0b2c88a235b4c9add6
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-08 12:30:17 +0300

    Create header component

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import { Button, Grid, Typography } from "@material-ui/core";
+
+export const Header = () => {
+    return(
+            <Grid
+                style={{ background: "#3295FF" }}
+                container
+                direction="row"
+                justify="space-between"
+                alignItems="center">
+            <Typography
+                style={{ padding: 20, color:"white" }}
+                variant="h3"
+                align="left">
+                Todo App
+            </Typography>
+            <Button
+                style={{ marginRight: 30, textTransform: "none", fontSize: 18, color: "white" }}>
+                Войти
+            </Button>
+        </Grid>
+    )
+}
