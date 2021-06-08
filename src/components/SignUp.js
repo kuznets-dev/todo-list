6e9f00399dc53cfe477afdc7657b281433cd2d14
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-08 17:28:56 +0300

    Add post request

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,10 +1,30 @@
 import { Button, Container, Grid, TextField, Typography } from '@material-ui/core';
+import axios from '../axiosConfig';
 import React, { useState } from 'react';
 
-function Auth() {
+function SignUp() {
+
+    const [user, setUser] = useState({ name: '', password: '' });
+
+    const signUp = async () => {
+        try {
+            await axios.post('/registration', {
+                name: user.name,
+                password: user.password
+            })
+            setUser({ name: '', password: '' });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const handleChange = (e) => {
+        setUser({...user, [e.target.name]: e.target.value});
+    }
+
     return (
         <div className="wrapper">
-            <form>
+            <form onSubmit={e => e.preventDefault()}>
                 <Container>
                     <Grid
                         container
@@ -18,9 +38,17 @@ function Auth() {
                             Регистрация
                     </Typography>
                         <TextField
+                            type="name"
+                            name="name"
+                            onChange={e => handleChange(e)}
+                            value={user.name}
                             style={{ marginTop: 30 }}
-                            label="E-mail" />
+                            label="Username" />
                         <TextField
+                            type="password"
+                            name="password"
+                            onChange={handleChange}
+                            value={user.password}
                             style={{ marginTop: 30 }}
                             label="Password" />
                     </Grid>
@@ -31,12 +59,18 @@ function Auth() {
                     justify="center"
                     align="center">
                         <Button
-                            style={{ marginRight: 30 }}
+                            onClick={() => signUp()}
+                            type="submit"
+                            style={{ marginRight: 30, textTransform: 'none' }}
                             variant="contained"
                             color="primary">
-                            Войти
+                            SignUp
                         </Button>
-                        <Button color="primary">Уже есть аккаунт?</Button>
+                        <Button
+                            style={{ textTransform: 'none' }}
+                            color="primary">
+                            Уже есть аккаунт?
+                        </Button>
                     </Grid>
                 </Container>
             </form>
@@ -44,4 +78,4 @@ function Auth() {
     )
 }
 
-export default Auth;
+export default SignUp;
