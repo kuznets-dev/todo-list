48f46265302bfe6f90527869d36a253bd0c99f72
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-08 13:30:14 +0300

    Create authorization component

---
+++ src/components/Auth.js
@@ -0,0 +1,47 @@
+import { Button, ButtonGroup, Container, Grid, TextField, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+
+function Auth() {
+    return (
+        <div className="wrapper">
+            <form>
+                <Container>
+                    <Grid
+                        container
+                        direction="column"
+                        justify="center"
+                        align="center">
+                        <Typography
+                            style={{ marginTop: 100 }}
+                            variant="h3"
+                            align="center">
+                            Авторизация
+                    </Typography>
+                        <TextField
+                            style={{ marginTop: 30 }}
+                            label="E-mail" />
+                        <TextField
+                            style={{ marginTop: 30 }}
+                            label="Password" />
+                    </Grid>
+                    <Grid
+                    style={{ marginTop: 30 }}
+                    container
+                    direction="row"
+                    justify="center"
+                    align="center">
+                        <Button
+                            style={{ marginRight: 30 }}
+                            variant="contained"
+                            color="primary">
+                            Войти
+                        </Button>
+                        <Button color="primary">Нет аккаунта?</Button>
+                    </Grid>
+                </Container>
+            </form>
+        </div>
+    )
+}
+
+export default Auth;
