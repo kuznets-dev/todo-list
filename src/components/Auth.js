1d7be6743a16f2b32876817285c1702f04220c82
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-10 10:20:29 +0300

    Fix the quotation marks

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,62 +2,62 @@ import { Button, Container, Grid, TextField, Typography } from '@material-ui/cor
 
 function Auth({ user, isSignup, setIsSignup, signUp, login, handleChange }) {
     return (
-        <div className="wrapper">
+        <div className='wrapper'>
             <form onSubmit={e => e.preventDefault()}>
                 <Container>
                     <Grid
                         container
-                        direction="column"
-                        justify="center"
-                        align="center">
+                        direction='column'
+                        justify='center'
+                        align='center'>
                         <Typography
                             style={{ marginTop: 100 }}
-                            variant="h3"
-                            align="center">
+                            variant='h3'
+                            align='center'>
                             {isSignup ? 'SignUp': 'Login'}
                     </Typography>
                         <TextField
-                            type="name"
-                            name="name"
+                            type='name'
+                            name='name'
                             onChange={handleChange}
                             value={user.name}
                             style={{ marginTop: 30 }}
-                            label="Username" />
+                            label='Username' />
                         <TextField
-                            type="password"
-                            name="password"
+                            type='password'
+                            name='password'
                             onChange={handleChange}
                             value={user.password}
                             style={{ marginTop: 30 }}
-                            label="Password" />
+                            label='Password' />
                     </Grid>
                     <Grid
                     style={{ marginTop: 30 }}
                     container
-                    direction="row"
-                    justify="center"
-                    align="center">
+                    direction='row'
+                    justify='center'
+                    align='center'>
                         {isSignup
                         ?<Button
                             onClick={signUp}
-                            type="submit"
+                            type='submit'
                             style={{ marginRight: 30, textTransform: 'none' }}
-                            variant="contained"
-                            color="primary">
+                            variant='contained'
+                            color='primary'>
                             SignUp
                         </Button>
                         :<Button
                             onClick={login}
-                            type="submit"
+                            type='submit'
                             style={{ marginRight: 30, textTransform: 'none' }}
-                            variant="contained"
-                            color="primary">
+                            variant='contained'
+                            color='primary'>
                             Login
                         </Button>}
                         <Button
                             onClick={() => setIsSignup(!isSignup)}
                             style={{ textTransform: 'none' }}
-                            color="primary">
+                            color='primary'>
                             {isSignup ? 'Do you have account?': 'No accaunt?'}
                         </Button>
                     </Grid>
